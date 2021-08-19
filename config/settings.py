b6c1de563b4d1f1f779fec0007e5ce506fdc970c
ronny karani <karanironny25@gmail.com>
2021-08-19 11:52:15 +0300

    contact pages

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sendemail.apps.SendemailConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEFAULT_FROM_EMAIL = 'karanironny25@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ readme.md
@@ -0,0 +1,5 @@
+Using Django's built-in [Forms API](https://docs.djangoproject.com/en/3.0/ref/forms/api/)
+
+Simple contact form that sends email for a Django website.
+
+Also uses Django's built-in [email support](https://docs.djangoproject.com/en/3.0/topics/email/) to make this relatively painless and then send the emails for real using  an email service of your choice.
